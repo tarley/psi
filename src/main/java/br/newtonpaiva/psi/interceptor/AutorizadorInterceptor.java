c7b7b0e2fedfdb0937d1bbd0b8fbcc280fde1b62
Mari Braga <mari-b@live.com>
2015-10-18 20:03:55 -0200

    Implementação de autenticação de usuário.

---
+++ src/main/java/br/newtonpaiva/psi/interceptor/AutorizadorInterceptor.java
@@ -0,0 +1,26 @@
+package br.newtonpaiva.psi.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object controller) 
+			  throws Exception {
+
+	      String uri = request.getRequestURI();
+	      if(uri.endsWith("login") || uri.endsWith("efetuaLogin") || uri.equals("/psi/") || uri.contains("resources")){
+	        return true;
+	      }
+
+	      if(request.getSession().getAttribute("usuarioLogado") != null) {
+	        return true;
+	      }
+
+	      response.sendRedirect("login");
+	      return false;
+	  }
+}
