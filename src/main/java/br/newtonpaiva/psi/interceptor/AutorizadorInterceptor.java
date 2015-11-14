71ac687dd5d71a40e8bc5e7b64f96bca19f043b7
Mari Braga <mari-b@live.com>
2015-11-14 18:50:45 -0200

    Correção login.

--- src/main/java/br/newtonpaiva/psi/interceptor/AutorizadorInterceptor.java
+++ src/main/java/br/newtonpaiva/psi/interceptor/AutorizadorInterceptor.java
@@ -13,7 +13,9 @@ public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
 
 	      String uri = request.getRequestURI();
 	      if(uri.endsWith("login") || uri.endsWith("efetuaLogin") || uri.equals("/psi/") ||
-	    		   uri.equals("")|| uri.contains("resources") || uri.contains("/pesquisar") || uri.contains("/listarBairros")){
+	    		   uri.contains("resources") || uri.contains("/pesquisar") 
+	    		   || uri.contains("/listarBairros") || uri.equals("www.projetopsi.com.br")
+	    		   || uri.equals("projetopsi.com.br")){
 	        return true;
 	      }
 
